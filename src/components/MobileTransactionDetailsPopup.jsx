aa0d1fa875021e9dbc44d11881c0f93e1f3c7a91
Israel Olorunsola <olorunsolaisrael353@gmail.com>
2021-10-29 21:32:19 +0100

    refactor: made popup into a component where you just add custom popup-content styles

--- src/components/MobileTransactionDetailsPopup.jsx
+++ src/components/MobileTransactionDetailsPopup.jsx
@@ -5,6 +5,8 @@ import amazonIcon from "../assets/images/icons/amazon.svg"
 import blankIcon from "../assets/images/blank-icon.svg"
 import copyIcon from "../assets/images/copy-icon.svg"
 
+import Popup from "./PopupContainer"
+
 const Unfailed = () => {
     return (
         <>
@@ -138,19 +140,16 @@ const MobileDetailsPopup = ({openPopup, handlePopupOpen}) => {
     return (
         <>
             {openPopup &&
-                <div className="mobile-history-detailbox">
-                    <div className="content-box">
-                        <button className="cancel-button" onClick={closePopup}>
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1.43129 8.27275C2.22757 4.87812 4.87812 2.22757 8.27275 1.43129C10.7243 0.856236 13.2757 0.856235 15.7273 1.43129C19.1219 2.22757 21.7724 4.87812 22.5687 8.27275C23.1438 10.7243 23.1438 13.2757 22.5687 15.7272C21.7724 19.1219 19.1219 21.7724 15.7272 22.5687C13.2757 23.1438 10.7243 23.1438 8.27276 22.5687C4.87812 21.7724 2.22757 19.1219 1.43129 15.7273C0.856235 13.2757 0.856235 10.7243 1.43129 8.27275Z" stroke="#3A6A95" stroke-width="2"/>
-                                <path d="M14 10L10 14M14 14L10 10" stroke="#3A6A95" stroke-width="2" stroke-linecap="round"/>
-                            </svg>
-                        </button>
+                <Popup className="mobile-history-detailbox">
+                    <button className="cancel-button" onClick={closePopup}>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.43129 8.27275C2.22757 4.87812 4.87812 2.22757 8.27275 1.43129C10.7243 0.856236 13.2757 0.856235 15.7273 1.43129C19.1219 2.22757 21.7724 4.87812 22.5687 8.27275C23.1438 10.7243 23.1438 13.2757 22.5687 15.7272C21.7724 19.1219 19.1219 21.7724 15.7272 22.5687C13.2757 23.1438 10.7243 23.1438 8.27276 22.5687C4.87812 21.7724 2.22757 19.1219 1.43129 15.7273C0.856235 13.2757 0.856235 10.7243 1.43129 8.27275Z" stroke="#3A6A95" stroke-width="2"/>
+                            <path d="M14 10L10 14M14 14L10 10" stroke="#3A6A95" stroke-width="2" stroke-linecap="round"/>
+                        </svg>
+                    </button>
 
-                        <Failed />
-                        
-                    </div>
-                </div>
+                    <Failed />
+                </Popup>    
             }
         </>
     )
---
+++ src/components/PopupContainer.jsx
@@ -0,0 +1,13 @@
+const Popup = ({className, children}) => {
+    return (
+        <div className={`popup ${className}`}>
+            <div className="scroll-container">
+                <div className="popup-content">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Popup;
