64bc1c7c0fd13f7cefd457e1f5496f9c7f4c7200
Israel Olorunsola <olorunsolaisrael353@gmail.com>
2021-11-11 21:01:43 +0100

    partial: transactions popup (desktop and mobile) - pagination left

--- src/views/transactions/index.jsx
+++ src/views/transactions/index.jsx
@@ -1,34 +1,39 @@
-import useWindowSize from "../../hooks/useWindowSize"
-import HistoryDesktop from "./HistoryDesktop"
-import HistoryMobile from "./HistoryMobile"
-import Footer from "../../layout/admin/Footer"
-import FundStat from "../../components/FundStat"
-import { PageHeading } from "../../components/Typography"
+import useWindowSize from "../../hooks/useWindowSize";
+import HistoryDesktop from "./HistoryDesktop";
+import HistoryMobile from "./HistoryMobile";
+import Footer from "../../layout/admin/Footer";
+import FundStat from "../../components/FundStat";
+import { PageHeading } from "../../components/Typography";
+
+import { useTrades } from "hooks";
 
 const Transactions = () => {
-    const windowSize = useWindowSize()
+	const windowSize = useWindowSize();
 
-    return (
-        <main className="transactions">
-            <div className="transactions__top">
-                <PageHeading text="Gift Card" span="History"/>
+	// get trades data from route called in useTrades hook
+	const tradesResponse = useTrades();
+	const trades = tradesResponse.data;
 
-                <FundStat/>
-            </div>
+	return (
+		<main className="transactions">
+			<div className="transactions__top">
+				<PageHeading text="Gift Card" span="History" />
 
+				<FundStat />
+			</div>
 
-            <div className="transactions__bottom">
-                {/* if window size is greater than 1199px, render desktop history and vice versa  */}
-                {windowSize.width > 1199 ? (
-                    <HistoryDesktop />
-                ) : (
-                    <HistoryMobile />
-                )}
-            </div>
+			<div className="transactions__bottom">
+				{/* if window size is greater than 1199px, render desktop history and vice versa  */}
+				{windowSize.width > 1199 ? (
+					<HistoryDesktop trades={trades} />
+				) : (
+					<HistoryMobile trades={trades} />
+				)}
+			</div>
 
-            <Footer />
-        </main>
-    )
-}
+			<Footer />
+		</main>
+	);
+};
 
-export default Transactions
+export default Transactions;
