f9304572dd1a5e7399ab699055563d603071f66a
Israel Olorunsola <olorunsolaisrael353@gmail.com>
2021-11-15 15:43:52 +0100

    finished: reset password page ui

--- src/views/reset-password/index.jsx
+++ src/views/reset-password/index.jsx
@@ -1,7 +1,47 @@
+import { BlackSubmit } from "components/Buttons";
+
 const ResetPassword = () => {
     return (
-        <div>ResetPassword</div>
-    )
+		<main className="sign reset-password">
+			<div className="sign-content reset-password-content">
+				<div className="sign-content-left reset-password-content-left">
+					<h1 className="header-text text-xl text-blue-dark text-vbold">
+                        Ouch!
+					</h1>
+
+					<h1
+						className="small-text text-24 text-bold"
+						style={{ color: "#CECECE" }}
+					>
+						Let us help you out.
+					</h1>
+
+					
+				</div>
+
+				<div className="sign-content-right reset-password-content-right">
+					<div className="formbox">
+						<form action="#">
+							<input
+								type="email"
+								className="default-input"
+								placeholder="Drop your email"
+								// {...register("email")}
+							/>
+
+                            <BlackSubmit text="Get me in" />
+						</form>
+					</div>
+
+					<h3 className="enter-email text-small text-regular text-blue-dark">
+                        Please enter your registered Email ID
+                        here. We will send a reset link to
+                        your email.
+					</h3>
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default ResetPassword
